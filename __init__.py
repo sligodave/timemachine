789ce71c9041aed1b0e82cbcaebbc434bc19d2c7
David Higgins <sligodave@gmail.com>
2014-01-28 15:03:37 -0700

    Split out the TimeMachine and InteractiveTimeMachine classes.

--- __init__.py
+++ __init__.py
@@ -1,3 +1,12 @@
+"""
+
+NOTE: You should really run this with root access as it will probably be
+needed if you want full access to the Time Machine harddrive.
+
+NOTE: This only supports files and directories.
+It skips over links or any other non file/directory items.
+
+"""
 
 import shutil
 import os
@@ -115,75 +124,6 @@ class TimeMachine(object):
                 return None
         return path
 
-    def interactive_select(self, choices, message='Please pick one of the following:', prompt=None):
-        print(message)
-        for i, choice in enumerate(choices):
-            print('{}. {}'.format(i, choice))
-        prompt = 'Enter 0-{}: '.format(len(choices) - 1) if prompt is None else prompt
-        return raw_input(prompt)
-
-    def interactive(self):
-        euid = os.geteuid()
-        if euid != 0:
-            print('IMPORTANT: It is recommended you run this as root\n'
-                  'Without it you may not have full access '
-                  'to the timemachine\'s files.\n'
-                  'Consider ctrl-C to start again.\n')
-        # Get Host
-        if self.hostname is None:
-            self.hostname = self.hostnames[
-                int(
-                    self.interactive_select(
-                        self.hostnames,
-                        'Select the hostname to use:'
-                    )
-                )
-            ]
-        # Get Version
-        if self.version is None:
-            self.version = self.versions[
-                int(
-                    self.interactive_select(
-                        self.versions,
-                        'Select the version to use:'
-                    )
-                )
-            ]
-        # Get Partition
-        if self.partition is None:
-            self.partition = self.partitions[
-                int(
-                    self.interactive_select(
-                        self.partitions,
-                        'Select the partition to use:'
-                    )
-                )
-            ]
-        # List directories on loop to walk. Copy a file or directory to dst
-        self.interactive_directory_select()
-
-    def interactive_directory_select(self):
-        path = '/'
-        while 1:
-            real_path = self.get_real_path(path)
-            choices = ['Quit', 'Copy directory {}'.format(path), '..'] + self.listdir(real_path)
-            message = 'Select a directory to enter or file to copy:'
-            choice = int(self.interactive_select(choices, message))
-            if choice == 0:
-                return
-            elif choice == 1:
-                self.interactive_copy_path(path)
-            elif choice == 2:
-                path = os.sep.join(path.split(os.sep)[:-1])
-            elif os.path.isfile(self.get_real_path(os.path.join(path, choices[choice]))):
-                self.interactive_copy_path(os.path.join(path, choices[choice]))
-            else:
-                path = os.path.join(path, choices[choice])
-
-    def interactive_copy_path(self, path):
-        destination_path = raw_input('Please enter a destination directory: ')
-        self.copy_path(path, destination_path)
-
     def copy_path(self, path, dest_path):
         """
         Given a path that would have worked on the backed up machine,
@@ -229,6 +169,91 @@ class TimeMachine(object):
         ]
 
 
+class InteractiveTimeMachine(TimeMachine):
+    def interact(self):
+        euid = os.geteuid()
+        if euid != 0:
+            print('IMPORTANT: It is recommended you run this as root\n'
+                  'Without it you may not have full access '
+                  'to the timemachine\'s files.\n'
+                  'Consider ctrl-C to start again.\n')
+        # Get Host
+        if self.hostname is None:
+            self.hostname = self.hostnames[
+                int(
+                    self.interactive_select(
+                        self.hostnames,
+                        'Select the hostname to use:'
+                    )
+                )
+            ]
+        # Get Version
+        if self.version is None:
+            self.version = self.versions[
+                int(
+                    self.interactive_select(
+                        self.versions,
+                        'Select the version to use:'
+                    )
+                )
+            ]
+        # Get Partition
+        if self.partition is None:
+            self.partition = self.partitions[
+                int(
+                    self.interactive_select(
+                        self.partitions,
+                        'Select the partition to use:'
+                    )
+                )
+            ]
+        # List directories on loop to walk. Copy a file or directory to dst
+        self.interactive_directory_select()
+
+    def interactive_select(self, choices, message='Please pick one of the following:', prompt=None):
+        print(message)
+        for i, choice in enumerate(choices):
+            print('{}. {}'.format(i, choice))
+        prompt = 'Enter 0-{}: '.format(len(choices) - 1) if prompt is None else prompt
+        return raw_input(prompt)
+
+    def interactive_directory_select(self):
+        path = '/'
+        while 1:
+            real_path = self.get_real_path(path)
+            choices = ['Quit', 'Copy directory {}'.format(path), '..'] + self.listdir(real_path)
+            message = 'Select a directory to enter or file to copy:'
+            choice = int(self.interactive_select(choices, message))
+            if choice == 0:
+                return
+            elif choice == 1:
+                self.interactive_copy_path(path)
+                break
+            elif choice == 2:
+                path = os.sep.join(path.split(os.sep)[:-1])
+            elif os.path.isfile(self.get_real_path(os.path.join(path, choices[choice]))):
+                self.interactive_copy_path(os.path.join(path, choices[choice]))
+                break
+            else:
+                path = os.path.join(path, choices[choice])
+
+    def interactive_copy_path(self, path):
+        destination_path = raw_input('Please enter a destination directory: ')
+        self.copy_path(path, destination_path)
+
+
 if __name__ == "__main__":
-    tm = TimeMachine(sys.argv[1])
-    tm.interactive()
+    if len(sys.argv) == 2:
+        tm = InteractiveTimeMachine(sys.argv[1])
+        tm.interact()
+    elif len(sys.argv) == 7:
+        tm = TimeMachine(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+        tm.copy_path(sys.argv[5], sys.argv[6])
+    else:
+        print(
+            'USAGE:\n'
+            'python {} HARDDRIVE_MOUNTED_PATH\n'
+            'OR\n'
+            'python {} HARDDRIVE_MOUNTED_PATH HOSTNAME VERSION '
+            'PARTITION MAC_SOURCE_PATH DESTINATION_DIRECTORY'.format(sys.argv[0], sys.argv[0])
+        )
